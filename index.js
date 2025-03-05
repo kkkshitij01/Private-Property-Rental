1a83e613eb7667050c761c6757f17eb1655f61b4
Kshitij Gupta <gkshitij0510@gmail.com>
2025-03-05 13:03:11 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,30 @@
+//basic setup
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("view", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+//Connecting to data base
+const MONGO_URL = "mongodb://127.0.0.1:27017/property-rental";
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+main()
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(8080, () => {
+  console.log("Listening to port 8080");
+});
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
